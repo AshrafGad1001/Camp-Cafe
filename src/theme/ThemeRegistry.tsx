26de82b4461dff34f225cd8ca7559dd33bee8e89
AshrafGad1001 <ashrafgad542@gmail.com>
2026-07-28 20:58:52 +0300

    Fix: Add prefixer to stylisPlugins to resolve RTL hydration mismatch

--- src/theme/ThemeRegistry.tsx
+++ src/theme/ThemeRegistry.tsx
@@ -7,10 +7,11 @@ import theme from './theme';
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
   return (
-    <AppRouterCacheProvider options={{ key: 'muirtl', stylisPlugins: [rtlPlugin] }}>
+    <AppRouterCacheProvider options={{ key: 'muirtl', stylisPlugins: [prefixer, rtlPlugin] }}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         {children}
