ab415c4ce88bdb7e6a4888b460683af24a83b00c
AshrafGad1001 <ashrafgad542@gmail.com>
2026-07-28 14:20:40 +0300

    Revert MenuItemCard layout: simple column for text with price at bottom

--- src/components/public/MenuItemCard.tsx
+++ src/components/public/MenuItemCard.tsx
@@ -36,7 +36,7 @@ export default function MenuItemCard({ name, description, price, image }: MenuIt
       </Box>
 
       {image?.url && (
-        <Box sx={{ width: 100, height: 100, flexShrink: 0 }}>
+        <Box sx={{ width: 100, height: 100, flexShrink: 0, ml: 2 }}>
           <CardMedia 
             component="img" 
             image={image.url} 
